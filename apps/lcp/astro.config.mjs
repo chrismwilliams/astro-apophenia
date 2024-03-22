119154fed1bd4113cf36f68cbd0d30d6958e9b25
Chris Williams <chrismwilliams@users.noreply.github.com>
2024-03-22 11:16:00 +0000

    update site url

--- apps/lcp/astro.config.mjs
+++ apps/lcp/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-image-lcp.pages.dev/",
+	site: "https://lcp.pages.dev/",
 	integrations: [],
 });
